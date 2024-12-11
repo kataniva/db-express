a23d30fa7c3d268f278a06c9eb08944e130b4370
Kata <kata.niva03@gmail.com>
2024-12-11 09:25:47 +0200

    express projecti

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
+var userRouter = require('./routes/user');
+
 
 var app = express();
 
@@ -15,6 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/user',userRouter);
 
 module.exports = app;
